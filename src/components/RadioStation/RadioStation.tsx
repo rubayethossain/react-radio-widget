4ebeff00fa32276f30ab646b77cb1e146c914555
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-06-19 21:56:52 +0600

    Make frequency text bold

--- src/components/RadioStation/RadioStation.tsx
+++ src/components/RadioStation/RadioStation.tsx
@@ -4,33 +4,34 @@ import { IconButton } from '../Buttons';
 import { StationName } from '../Typography';
 import styles from './RadioStation.module.css';
 
-type Props = Station & {    
-    activeStation: number | null | undefined
-}
+type Props = Station & {
+    activeStation: number | null | undefined;
+};
 
 const RadioStation = (props: Props) => {
-    const {id, frequency, name, activeStation} = props;
+    const { id, frequency, name, activeStation } = props;
 
     const clickHandler = () => {};
 
     const stationPlayer = (
         <div className={styles.player}>
-            <IconButton icon={PLUS_ICON} onClick={clickHandler}/>
+            <IconButton icon={PLUS_ICON} onClick={clickHandler} />
             <img src={STATION_ICON} alt="" />
-            <IconButton icon={MINUS_ICON} onClick={clickHandler}/>
+            <IconButton icon={MINUS_ICON} onClick={clickHandler} />
         </div>
-    )
+    );
 
     return (
         <section>
             {activeStation === id && stationPlayer}
             <div className={styles.radio}>
                 <StationName>{name}</StationName>
-                <StationName>{frequency}</StationName>
+                <StationName style={{ fontWeight: 'bold' }}>
+                    {frequency}
+                </StationName>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default RadioStation
-
+export default RadioStation;
