843ac5ecca30fe20b9b48d3138cc338c1a41a441
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-06-19 21:48:02 +0600

    Create hook for fetching stations

---
+++ src/hooks/useFetchStations.ts
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { Station } from '../screens/RadioWidget/types';
+
+const DEFAULT_STATIONS = [
+    {
+        id: 1,
+        name: 'Putin FM',
+        frequency: '66,6',
+    },
+    {
+        id: 2,
+        name: 'Dribble FM',
+        frequency: '101,2',
+    },
+    {
+        id: 3,
+        name: 'Doge FM',
+        frequency: '99,4',
+    },
+    {
+        id: 4,
+        name: 'Ballads FM',
+        frequency: '87,1',
+    },
+    {
+        id: 5,
+        name: 'Maximum FM',
+        frequency: '142,2',
+    },
+];
+
+const useFetchStations = (stationEndpoint: string | null | '') => {
+    const [stations, setStations] = React.useState<Station[]>(DEFAULT_STATIONS);
+
+    React.useEffect(() => {
+        const fetchStations = async (endpoint: string) => {
+            try {
+                const req = await fetch(endpoint, { method: 'GET' });
+                const res = await req.json();
+
+                if(Array.isArray(res) && res.length){
+                    setStations(res);
+                }
+            } catch (error) { console.log({ error })}
+        };
+
+        if (stationEndpoint) fetchStations(stationEndpoint);
+    }, [stationEndpoint]);
+
+    return { stations };
+};
+
+export default useFetchStations;
--- src/screens/RadioWidget/RadioWidget.tsx
+++ src/screens/RadioWidget/RadioWidget.tsx
@@ -4,34 +4,7 @@ import WidgetContent from './WidgetContent';
 import WidgetFooter from './WidgetFooter';
 import * as React from 'react';
 import { Station } from './types';
-
-const STATIONS = [
-    {
-        id: 1,
-        name: 'Putin FM',
-        frequency: '66,6',
-    },
-    {
-        id: 2,
-        name: 'Dribble FM',
-        frequency: '101,2',
-    },
-    {
-        id: 3,
-        name: 'Doge FM',
-        frequency: '99,4',
-    },
-    {
-        id: 4,
-        name: 'Ballads FM',
-        frequency: '87,1',
-    },
-    {
-        id: 5,
-        name: 'Maximum FM',
-        frequency: '142,2',
-    },
-];
+import useFetchStations from '../../hooks/useFetchStations';
 
 const INITIAL_STATION: Station = {
     id: null,
@@ -44,11 +17,13 @@ const RadioWidget = () => {
         Object.assign({}, INITIAL_STATION)
     );
 
+    const {stations} = useFetchStations(null);
+
     return (
         <div className={styles.wrapper}>
             <WidgetTopBar />
             <WidgetContent
-                stationsList={STATIONS}
+                stationsList={stations}
                 activeStation={activeStation}
                 setActiveStation={setActiveStation}
             />
