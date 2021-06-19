206da28284be5c9ac5d526e4727d5a2fcf4bf41b
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-06-19 11:36:43 +0600

    Create folder and add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+import BACK_ARROW_ICON from './img/back-arrow.png';
+import MINUS_ICON from './img/minus.png';
+import PLUS_ICON from './img/plus.png';
+import STATION_ICON from './img/station.png';
+import SWITCH_ICON from './img/switch.png';
+
+export {
+    BACK_ARROW_ICON, 
+    MINUS_ICON,
+    PLUS_ICON,
+    STATION_ICON,
+    SWITCH_ICON
+}
