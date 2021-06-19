8b999fece931339372ca257b39b6a9ec744bfdc6
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-06-19 16:53:54 +0600

    Design Top Bar

--- src/screens/RadioWidget/WidgetTopBar/WidgetTopBar.tsx
+++ src/screens/RadioWidget/WidgetTopBar/WidgetTopBar.tsx
@@ -3,11 +3,15 @@ import { IconButton } from '../../../components/Buttons';
 import styles from './WidgetTopBar.module.css';
 
 const WidgetTopBar = () => {
+    const clickHandler = () => {};
     return (
-        <div className={styles.wrapper}>
-            <IconButton icon={BACK_ARROW_ICON} onClick={() => console.log()} />
-            <span>STATIONS</span>
-            <IconButton icon={SWITCH_ICON} onClick={() => console.log()} />
+
+        <div className={styles.wrapper}>        
+            <IconButton icon={BACK_ARROW_ICON} onClick={clickHandler} height="28px"/>
+
+            <p className={styles.title}>STATIONS</p>
+            
+            <IconButton icon={SWITCH_ICON} onClick={clickHandler} height="28px"/>
         </div>
     )
 }
